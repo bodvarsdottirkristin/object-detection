682b515c78023c1431703c7818abc1e655b8ddad
linusjuni <linus@juni.dk>
2025-12-06 16:54:19 +0100

    small changes to model

--- src/models/pothole_classifier.py
+++ src/models/pothole_classifier.py
@@ -9,11 +9,9 @@ logger = get_logger(__name__)
 
 
 class PotholeClassifier(nn.Module):
-    def __init__(self, num_classes=2, pretrained=True):
-        """Pothole Classifier using ResNet18 backbone. Takes number of classes and pretrained flag as input."""
+    def __init__(self, num_classes=2, pretrained=True, freeze_backbone=False, dropout_p=0.5):
         super(PotholeClassifier, self).__init__()
         
-        # 1. Load the backbone
         if pretrained:
             self.backbone = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
             logger.info("Loaded ResNet18 with pretrained weights.")
@@ -21,9 +19,18 @@ class PotholeClassifier(nn.Module):
             self.backbone = models.resnet18(weights=None)
             logger.info("Loaded ResNet18 without pretrained weights.")
 
-        # 2. Modify the classifier head        
+        if freeze_backbone:
+            for param in self.backbone.parameters():
+                param.requires_grad = False
+            for param in self.backbone.fc.parameters():
+                param.requires_grad = True
+            logger.info("Backbone frozen except final layer.")
+
         in_features = self.backbone.fc.in_features
-        self.backbone.fc = nn.Linear(in_features, num_classes)
+        self.backbone.fc = nn.Sequential(
+            nn.Dropout(dropout_p),
+            nn.Linear(in_features, num_classes)
+        )
 
     def forward(self, x):
         return self.backbone(x)
